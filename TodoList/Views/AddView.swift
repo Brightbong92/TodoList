5a8783116bcd6407ac70c8a3528d05e8c925584c
tirrilee mac mini <wkdgusqhd080@naver.com>
2022-06-07 15:30:09 +0900

    add CRUD 기능 추가

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -2,35 +2,60 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText : String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     let myGray = Color(#colorLiteral(red: 0.9403803662, green: 0.9403803662, blue: 0.9403803662, alpha: 1))
     
     var body: some View {
         ScrollView {
             VStack {
-                TextField("Type seomthing hre...", text: $textFieldText)
+                TextField("할일을 적어주세요..", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
                     .background(myGray)
                     .cornerRadius(10)
                 
-                Button(action: {
-                    
-                }, label: {
-                    Text("Save".uppercased())
+                Button(action: saveButtonPressed
+                , label: {
+                    Text("저장".uppercased())
                         .foregroundColor(.white)
                         .frame(height:55)
                         .frame(maxWidth: .infinity)
                         .background(Color.accentColor)
                         .cornerRadius(10)
-                }
-                )
+                })
             }
             .padding(14)
         }
-        .navigationTitle("Add an Item ✏️")
+        .navigationTitle("할일 추가 ✏️")
+        .alert(isPresented: $showAlert, content: getAlert)
     }
+    
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsAppropriate () -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "최소 3글자 이상을 입력해주세요. 😵😥"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert () -> Alert {
+        return Alert(title: Text(alertTitle))
+    }
+    
 }
 
 struct AddView_Previews: PreviewProvider {
@@ -38,5 +63,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
